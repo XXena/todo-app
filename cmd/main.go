10fe9bc66defa1d340a84320e785f72c5df9ceb7
XXena <kvvolkova@gmail.com>
2021-04-16 22:29:14 +0300

    clean architecture, dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/XXena/todo-app"
 	"github.com/XXena/todo-app/package/handler"
+	"github.com/XXena/todo-app/package/repository"
+	"github.com/XXena/todo-app/package/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
