e98914608f5d01eda8ffd4c32c067e08ec5d48c4
XXena <kvvolkova@gmail.com>
2021-04-17 11:09:20 +0300

    added postgres

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/XXena/todo-app/package/handler"
 	"github.com/XXena/todo-app/package/repository"
 	"github.com/XXena/todo-app/package/service"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
 )
@@ -14,7 +15,20 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
