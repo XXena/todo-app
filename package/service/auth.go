e6042aafa8c7ff1ea90a60b87248400c4c235656
XXena <kvvolkova@gmail.com>
2021-04-17 12:28:24 +0300

    auth: add create user - repository and service levels

---
+++ package/service/auth.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"github.com/XXena/todo-app"
+	"github.com/XXena/todo-app/package/repository"
+)
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	return s.repo.CreateUser(user)
+}
