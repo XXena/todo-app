40622e889fd39a550c5ffe6d929c64e216aebe93
XXena <kvvolkova@gmail.com>
2021-04-18 18:56:37 +0300

    todo items: CRUD

--- todo.go
+++ todo.go
@@ -15,13 +15,14 @@ type UserList struct {
 }
 
 type TodoItem struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Done        string `json:"done"`
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
+	Done        bool   `json:"done" db:"done"`
 }
 
-type ListsItem struct {
+// ListsItems связь список - элемент
+type ListsItems struct {
 	Id     int
 	ListId int
 	ItemId int
@@ -34,6 +35,20 @@ type UpdateListInput struct {
 
 func (i *UpdateListInput) Validate() error {
 	if i.Title == nil && i.Description == nil {
+		return errors.New("update structure has no values")
+	}
+
+	return nil
+}
+
+type UpdateItemInput struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	Done        *bool   `json:"done" db:"done"`
+}
+
+func (i *UpdateItemInput) Validate() error {
+	if i.Title == nil && i.Description == nil && i.Done == nil {
 		return errors.New("update structure has no values")
 	}
 
